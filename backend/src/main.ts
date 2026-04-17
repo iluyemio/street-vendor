4bda5788e6e7f967a1b0b2e276a7b5aa4e435f14
kelvinTechies <iamkelvincole@gmail.com>
2026-04-17 14:14:36 +0100

    solved 413 error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,14 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
     forbidNonWhitelisted:true,
